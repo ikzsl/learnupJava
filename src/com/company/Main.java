579ab52d247f9504a02bda2463f821520c7b26ba
ikzsl <ikzsl@yandex.ru>
2021-11-03 01:16:20 +0300

    task1.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,26 @@ package com.company;
 
 public class Main {
 
+
+    public static String exclaimGluier(String str) {
+        char exclaim = '\u0021';
+        return str + exclaim;
+    }
+
+    public static String hobbyGenerator(String hobby) {
+        return "My hobby is " + hobby;
+    }
+
     public static void main(String[] args) {
-	System.out.println("abrakadabra");
+        byte myAge = 123;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Hi");
+        stringBuilder.append("\n");
+        stringBuilder.append("I am ");
+        stringBuilder.append(myAge);
+        stringBuilder.append(" years old");
+        stringBuilder.append("\n");
+        stringBuilder.append(hobbyGenerator("shashlik eating"));
+        System.out.println(exclaimGluier(stringBuilder.toString()));
     }
 }
